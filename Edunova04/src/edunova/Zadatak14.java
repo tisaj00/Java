19b0cf0a3f1bbfe926b0b6cb26e851e4eceb0695
tisaj00 <josip.tisaj00@gmail.com>
2018-12-14 11:37:36 +0100

    vježba

--- Edunova04/src/edunova/Zadatak14.java
+++ Edunova04/src/edunova/Zadatak14.java
@@ -1,10 +1,26 @@
 package edunova;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 public class Zadatak14 {
+	public static void main(String[] args) {
+		
+
 
 	//Program prima logičku vrijednost
 	//us slučaju istinitosti ispisuje Osijek
 	//inače ispisuje Slučajni broj između 0 i 1
 	
+	boolean x = Boolean.parseBoolean(JOptionPane.showInputDialog("Unesite logičku vrijednost:"));
 	
+	if(x){
+		System.out.println("Osijek");
+		
+	}
+	if(!x){
+		System.out.println("broj: "+Math.random());
+	}
+}
 }
--- Edunova04/src/edunova/Zadatak9.java
+++ Edunova04/src/edunova/Zadatak9.java
@@ -1,10 +1,32 @@
 package edunova;
 
+import javax.swing.JOptionPane;
+
 public class Zadatak9 {
 
 	//U programu omogućite unos broja sekundi i ispišite 
 	//odgovarajuće vrijeme u satima, minutama i sekundama
 	
+	public static void main(String[] args) {
+		
+		int i = Integer.parseInt(JOptionPane.showInputDialog("Unesite broj sekundi:"));
+		int p1,p2,p3;
+		p1=i%60;
+		p2=i/60;
+		p3=p2/60;
+		p2=p2/60;
+		
+		
+		
+			
+			System.out.println(p2+ "sat  "+ p3 + "minuta "+  p1+ "sekundi ");
+		
+		
+		
+		
+		
+		
+	}
 	
 	
 }
