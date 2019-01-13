2d9b8ead0114df0da77dea79d1922a76b638f63c
Josip <Josip@192.168.117.1>
2019-01-13 22:36:41 +0100

    Vjezba

---
+++ Vjezba/src/edunova/ModelAuta.java
@@ -0,0 +1,22 @@
+package edunova;
+
+public class ModelAuta {
+
+	private String Model;
+	private int godinaProizvodnje;
+	
+	public String getModel() {
+		return Model;
+	}
+	public void setModel(String model) {
+		Model = model;
+	}
+	public int getGodinaProizvodnje() {
+		return godinaProizvodnje;
+	}
+	public void setGodinaProizvodnje(int godinaProizvodnje) {
+		this.godinaProizvodnje = godinaProizvodnje;
+	}
+	
+	
+}
--- Vjezba/src/edunova/StartAuto.java
+++ Vjezba/src/edunova/StartAuto.java
@@ -7,12 +7,18 @@ public class StartAuto {
 		Auto a;
 		a= new Auto();
 		
+		ModelAuta b;
+		b = new ModelAuta();
+		
 		a.setJacina(110);
 		a.setBrojVrata(5);
 		a.setJakostMotora(1.6);
 		a.setVlasnik("Josip");
+		b.setGodinaProizvodnje(1995);
+		b.setModel("Golf");
 		
 		System.out.println(a.getVlasnik() + " ima auto jačine " + a.getJacina() + " jakosti motora " + a.getJakostMotora() + " i s brojem vrata "+ a.getBrojVrata());
+		System.out.println(b.getModel() + " je proizveden " + b.getGodinaProizvodnje());
 	}
 
 }
